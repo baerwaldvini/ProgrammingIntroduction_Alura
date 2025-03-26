dc670f90dbdaeef716c7c2bb8075001dffc24f66
Vinícius Leonardo Baerwald <vynyleo@gmail.com>
2025-03-26 16:50:07 -0300

    Ajustando arquivos para aula seguida do vídeo

---
+++ Aulas/Aula 3 - Desafios Praticos/Módulo 1 - Projeto Sorteador de Números/Execução Sozinho/3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -0,0 +1,39 @@
+//sozinho consegui desenvolver a lógica, só não consegui ajustar a fonte do HTML após sobreescrever o texto, será ajustado com os videos
+let numerosSorteados = []
+
+// Função que pede uma tag e texto e retorna o valor informado
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.getElementById(tag)
+  campo.innerHTML = texto
+}
+
+function sortear() {
+  let quantidade = parseInt(document.getElementById('quantidade').value)
+  let de = parseInt(document.getElementById('de').value)
+  let ate = parseInt(document.getElementById('ate').value)
+
+  while (quantidade > 0) {
+    let aleartorioNumber = renderNumber(de, ate)
+    if (numerosSorteados.includes == aleartorioNumber) {
+      aleartorioNumber = renderNumber(de, ate)
+    } else {
+      numerosSorteados.push(aleartorioNumber)
+    }
+    quantidade--
+    console.log(numerosSorteados)
+  }
+  document.getElementById('btn-reiniciar').removeAttribute('disabled')
+  return exibirTextoNaTela(
+    'resultado',
+    'Números sorteados: ' + numerosSorteados
+  )
+}
+
+function renderNumber(min, max) {
+  return parseInt(Math.random() * (max - min + 1)) + min
+}
+
+function reiniciar() {
+  numerosSorteados = []
+  exibirTextoNaTela('resultado', 'Números sorteados:  nenhum até agora')
+}
